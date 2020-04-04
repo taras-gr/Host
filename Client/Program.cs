c3e61101e757d3b45e9a784ebb560e80687dcc1d
taras-gr <tar.grytsenko@gmail.com>
2020-04-04 17:52:56 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class MyCallback : Proxy.IChatServiceCallback
+    {
+        public void RecieveMessage(string user, string message)
+        {
+            Console.WriteLine("{0}:{1}", user, message);
+            Program.Message = String.Format("{0}:{1}", user, message);
+        }
+    }
+    class Program
+    {
+        public static string Message;
+        static void Main(string[] args)
+        {
+            InstanceContext context = new InstanceContext(new MyCallback());
+            Proxy.ChatServiceClient server = new Proxy.ChatServiceClient(context);
+
+            Console.WriteLine("Enter UserName");
+            var username = Console.ReadLine();
+            server.Join(username);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter message");
+            Console.WriteLine("Press Q to exit");
+
+            var message = Console.ReadLine();
+
+            while(message != "Q")
+            {
+                if (!string.IsNullOrEmpty(message))
+                    server.SendMessage(message);
+
+                message = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Host/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host
+{
+    [ServiceContract]
+    public interface IChatClient
+    {
+        //int Length { get; set; }
+        [OperationContract(IsOneWay = false)]
+        void RecieveMessage(string user, string message);
+    }
+
+    [ServiceContract(CallbackContract = typeof(IChatClient))]
+    public interface IChatService
+    {
+        [OperationContract(IsOneWay = false)]
+        bool Join(string userName);
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string message);
+    }
+
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Single,
+        InstanceContextMode = InstanceContextMode.Single)]
+    public class ChatService : IChatService
+    {
+        Dictionary<IChatClient, string> _users = new Dictionary<IChatClient, string>();
+
+        public bool Join(string userName)
+        {
+            try
+            {
+                var connection = OperationContext.Current
+                .GetCallbackChannel<IChatClient>();
+                _users[connection] = userName;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            
+        }
+
+        public void SendMessage(string message)
+        {
+            var connection = OperationContext.Current
+                .GetCallbackChannel<IChatClient>();
+            string user;
+            if (!_users.TryGetValue(connection, out user))
+                return;
+            foreach (var other in _users.Keys)
+            {
+                if (other == connection)
+                    continue;
+                other.RecieveMessage(user, message);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(ChatService));
+            host.Open();
+            Console.WriteLine("Service is ready");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
